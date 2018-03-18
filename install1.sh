b6c3c1ec47e9caea305c253131c16777a36144df
sonic2005FILES <kubuyopa@doanart.com>
2018-03-18 10:25:21 +0200

    Add files via upload

---
+++ install1.sh
@@ -0,0 +1,7 @@
+apt-get install curl
+apt-get install -y build-essential
+apt-get install -y python-software-properties
+apt-get install -y php5
+apt-get install -y apache2
+apt-get install -y libapache2-mod-php5
+apt-get install -y mysql-server
